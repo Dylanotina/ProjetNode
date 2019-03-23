ec046255c46894a359134f18f638b64b3af119df
Maxime <maxime.guigourez@gmail.com>
2019-03-23 17:27:21 +0100

    ville search style

--- client/src/Pages_installation/Installation_RechercheVille.js
+++ client/src/Pages_installation/Installation_RechercheVille.js
@@ -35,8 +35,12 @@ class Installation_RechercheVille extends React.Component{
         return (<div id="tableau-installation">
 
                 <h2> Recherche par Ville </h2>
+                <a href="http://localhost:3000/"><button class="btn btn-secondary"> Accueil </button></a>
+                <br/>
+                <br/>
                 <label>Nom de la ville : </label><input value={this.state.inputValue} onChange={evt => this.updateInputValue(evt)}/>
                 <button class="chercher" onClick={this.handleSearchVille}> CHARGER</button>
+
                 <br/>
                 <br/>
                 <table class="table table-hover">
