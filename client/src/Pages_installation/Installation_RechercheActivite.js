2dce249c0ecc24df9eddb2ccf4db68f5355697fe
Maxime <maxime.guigourez@gmail.com>
2019-03-23 20:35:37 +0100

    finition style

--- client/src/Pages_installation/Installation_RechercheActivite.js
+++ client/src/Pages_installation/Installation_RechercheActivite.js
@@ -1,6 +1,8 @@
 import React from "react";
 import Client from "../Client";
-
+import './../css/bootstrap1.min.css'
+import './../css/ville.css';
+import {Link} from "react-router-dom";
 
 class Installation_RechercheActivite extends React.Component{
     state ={
@@ -35,7 +37,7 @@ class Installation_RechercheActivite extends React.Component{
     render() {
 
         const foodRows = this.state.installations.map((installation,i)=>(
-            <tr>
+            <tr class ="table-dark">
                 <td key={i}>{installation.noDeLInstallation}</td>
                 <td key={i}>{installation.nomUsuelDeLInstallation}</td>
                 <td key={i}>{installation.codePostal}</td>
@@ -46,13 +48,17 @@ class Installation_RechercheActivite extends React.Component{
             <option key={i}>{activite.activiteLibelle}</option>
         ))
         return (<div id="tableau-installation">
-                <select onChange={evt => this.updateInputValue(evt)}>
+                <h2> Reherche par Activité </h2> <br/>
+                <a href={"http://localhost:3000/"}><button class="btn btn-secondary"> Accueil</button></a> <br/><br/>
+
+                <select class="custom-select" onChange={evt => this.updateInputValue(evt)}>
                     {listAct}
                 </select>
-                <button onClick={this.handleSearchActivite}>Chercher</button>
-                <table>
+                <button class="chercher" onClick={this.handleSearchActivite}>Chercher</button>
+                <br/><br/>
+                <table class="table table-hover">
                     <thead>
-                    <tr>
+                    <tr class="table-warning">
                         <th>Numéro de l'installation</th>
                         <th>Nom de l'installation</th>
                         <th>Code Postal</th>
